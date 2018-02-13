d5b1a65006d2a5ed62e232f9c5db690c206bcf07
Libor Gabaj <libor.gabaj@gmail.com>
2018-02-13 21:56:19 +0100

    Added installation script for checking library.
    - Language files updated.

--- gbj.php
+++ gbj.php
@@ -1,25 +1,29 @@
 <?php
 /**
- * @package    Joomla.Component
- * @subpackage  System.Gbj
- * @copyright  (c) 2017 Libor Gabaj. All rights reserved.
- * @license     GNU General Public License version 2 or later; see LICENSE.txt
- * @since      3.7
+ * System plugin for loading the library LIB_GBJ
+ *
+ * @package    Joomla.Plugin
+ * @subpackage System.Gbj
+ * @copyright  (c)2017 Libor Gabaj
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * @since      3.8
  */
 
 defined('_JEXEC') or die;
 
 /**
- * System plugin to load custom library and layouts.
+ * System plugin definition class
  *
- * @since  3.7
+ * @package  Joomla.Class
+ * @license  GNU General Public License version 2 or later; see LICENSE.txt
+ * @since    3.8
  */
 class PlgSystemGbj extends JPlugin
 {
 	/**
 	 * Method to catch the onAfterInitialise event and register custom library.
 	 *
-	 * @return  void
+	 * @return void
 	 */
 	public function onAfterInitialise()
 	{
---
+++ gbj.script.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Installation script
+ *
+ * @package    Joomla.Plugin
+ * @subpackage System.Gbj
+ * @copyright  (c)2018 Libor Gabaj
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * @since      3.8
+ */
+
+defined('_JEXEC') or die();
+
+
+/**
+ * Installation class
+ *
+ * @license  GNU General Public License version 2 or later; see LICENSE.txt
+ * @since    3.8
+ */
+class PlgSystemGbjInstallerScript
+{
+	/**
+	 * Called before any type of action
+	 *
+	 * @param   string           $route   Which action is happening
+	 *                                    (install|uninstall|discover_install)
+	 * @param   jadapterinstance $adapter The object running this script
+	 *
+	 * @return boolean True on success
+	 */
+	public function preflight($route, JAdapterInstance $adapter)
+	{
+		if (!JLibraryHelper::isEnabled('gbj'))
+		{
+			JFactory::getApplication()->enqueueMessage(
+				JText::_('PLG_SYSTEM_GBJ_ERROR_NO_LIBRARY'),
+				'error'
+			);
+
+			return false;
+		}
+
+		return true;
+	}
+}
