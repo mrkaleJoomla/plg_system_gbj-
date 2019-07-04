bed5a87e7c10d85e8f2efb8600b8bad2021b3158
Libor Gabaj <gabaj.libor@slsp.sk>
2019-07-04 12:36:08 +0200

    Added check for installed custom library.

--- gbj.php
+++ gbj.php
@@ -4,7 +4,7 @@
  *
  * @package    Joomla.Plugin
  * @subpackage System.Gbj
- * @copyright  (c)2017 Libor Gabaj
+ * @copyright  (c)2017-2019 Libor Gabaj
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  * @since      3.8
  */
@@ -27,11 +27,16 @@ class PlgSystemGbj extends JPlugin
 	 */
 	public function onAfterInitialise()
 	{
-		$classPrefix = ucfirst($this->_name);
-		$libFolder = DIRECTORY_SEPARATOR . $this->_name;
-		$libName = 'lib_' . $this->_name;
+		// Load library with the same name as this plugin has
+		$library = $this->_name;
+		$classPrefix = ucfirst($library);
+		$libName = 'lib_' . $library;
+		$libFolder = realpath(JPATH_LIBRARIES . DIRECTORY_SEPARATOR . $library);
 
-		JLoader::registerPrefix($classPrefix, JPATH_LIBRARIES . $libFolder);
-		JFactory::getLanguage()->load($libName, JPATH_SITE);
+		if (file_exists($libFolder))
+		{
+			JLoader::registerPrefix($classPrefix, $libFolder);
+			JFactory::getLanguage()->load($libName, JPATH_SITE);
+		}
 	}
 }
